8fddab469951b1b692e760342cc444adad7ea944
Pratham Patel <ppratham134patel@gmail.com>
2025-09-26 17:09:20 +0530

    fix: update Mapbox token reference to use MAP_TOKEN variable

--- app.js
+++ app.js
@@ -117,5 +117,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`); 
 });
--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing.js");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+const geocodingClient = mbxGeocoding({ accessToken: process.env.mapToken });
 
 module.exports.index = async (req, res) => {
     const allListings = await  Listing.find({});
