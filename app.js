0d1d1a79a69349a316f79cb443ce61e9d54366c3
Pratham Patel <ppratham134patel@gmail.com>
2025-09-26 16:31:52 +0530

    fix: use process.env.PORT for Render

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use((err, req, res, next) => {
 //   console.log('Server is listening to port 8080');
 // });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
